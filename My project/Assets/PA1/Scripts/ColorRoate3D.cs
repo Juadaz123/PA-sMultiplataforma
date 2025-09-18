4a875d8c8af18c5710dabe48284ea93a0ca28454
Juadaz123 <160087791+Juadaz123@users.noreply.github.com>
2025-09-18 05:44:31 -0500

    3d Behaviour
    comportamiento de rotación y color en los objetos 3d

--- My project/Assets/PA1/Scripts/ColorRoate3D.cs
+++ My project/Assets/PA1/Scripts/ColorRoate3D.cs
@@ -1,18 +1,33 @@
+using System.Collections;
 using UnityEngine;
 
 public class ColorRoate3D : MonoBehaviour
 {
     
-    [(Header)]
+    [Header("3d parammeters")]
+    private Renderer _rendererObject;
+    [SerializeField] private Color startColor;
+    [SerializeField] private float speedRotation = 20f;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //inicializa la rotacion y cambia el color al iniciar la escena
+        _rendererObject = gameObject.GetComponent<Renderer>();
+        _rendererObject.material.color = startColor;
+
+        StartCoroutine(ObjectRotation());
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator ObjectRotation()
     {
-        
+        while (true)
+        {
+            transform.RotateAround(transform.position, Vector3.up, speedRotation * Time.deltaTime);
+            yield return null;  
+        }
     }
+
+    
 }
